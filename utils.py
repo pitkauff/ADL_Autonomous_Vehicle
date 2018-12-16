c688a880fef0dd5e4f0aae319ac703c8f68804e4
Manksh Gupta <mankshgupta@dyn-160-39-250-143.dyn.columbia.edu>
2018-12-16 14:21:16 -0500

    add functions to utils

--- utils.py
+++ utils.py
@@ -1,15 +1,11 @@
-"""
-Author: Manksh Gupta
-
-Stay Hungry. Stay Foolish.
-
-"""
 
 import os
 from PIL import Image
 import numpy as np
 import cv2
 import re
+from matplotlib import pyplot as plt
+from tensorflow.keras import backend as K
 
 def sorted_nicely(l):
     """ Sorts the given iterable in the way that is expected.
@@ -24,6 +20,15 @@ def sorted_nicely(l):
 
 
 def absSobelThresh(img, orient, thresh, sobelKernel=19):
+
+    """
+
+    :param img: image
+    :param orient: orientation
+    :param thresh: thresholds
+    :param sobelKernel: size of the filter
+    :return: binary image output
+    """
     threshMin = thresh[0]
     threshMax = thresh[1]
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -63,26 +68,48 @@ def combinedThreshBinaryImg(img, threshX, threshY, threshColorS, threshColorU, t
     return combinedBinary
 
 
-def load_data():
-    images = []
-    for files in sorted_nicely(os.listdir('training_images/')):
-        try:
-            mypath = os.path.join('training_images', files)
-            img = Image.open(mypath, mode='r')
-            img = img.resize((128, 128))
-            arr = np.array(img).astype('uint8')
-            arr = combinedThreshBinaryImg(arr, threshX=(1, 255),
-                                          threshY=(50, 255),
-                                          threshColorS=(1, 255),
-                                          threshColorU=(250, 250),
-                                          threshColorR=(230, 255))
-            arr = arr[60:, ]
-            images.append(arr)
-            img.close()
-        except:
-            pass
+def heatmap(images, model):
 
-    return images
+    """
+    This function creates a heatmap of the convolution layers and helps to visualise what the model is learning.
+    :param images: Image
+    :param model: model that is trained
+    :return: heatmap of filter
+    """
+    final_images = []
+    k = 1
+    for i in range(len(images)):
+        fig = plt.figure(figsize=(20, 6 * len(images)))
+        im = np.expand_dims(images[i], axis=0)
+        preds = model.predict(im)
+        output = model.get_layer("dense_2").output[:, np.argmax(preds)]
+        last_conv_layer = model.get_layer('conv2d_2')
+        grads = K.gradients(output, last_conv_layer.output)[0]
+        pooled_grads = K.mean(grads, axis=(0, 1, 2))
+        iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
+        grads_value, conv_layer_output_value = iterate([im])
+        for j in range(conv_layer_output_value.shape[2]):
+            conv_layer_output_value[:, :, j] *= grads_value[j]
+        heatmap_1 = np.mean(conv_layer_output_value, axis=- 1)
+        heatmap_1 = np.maximum(heatmap_1, 0)
+        heatmap_1 /= np.max(heatmap_1)
+        fig.add_subplot(len(images), 3, k)
+        plt.imshow(heatmap_1)
+
+        heatmap_2 = cv2.resize(heatmap_1, (images[i].shape[1], images[i].shape[0]))
+        heatmap_2 = np.uint8(255 * heatmap_2)
+        heatmap_2 = cv2.applyColorMap(heatmap_2, cv2.COLORMAP_JET)
+        fig.add_subplot(len(images), 3, k + 1)
+        plt.imshow(heatmap_2)
+
+        superimposed_img = heatmap_2 * 0.7 + images[i] * 255
+        superimposed_img = np.array(superimposed_img).astype('uint8')
+        fig.add_subplot(len(images), 3, k + 2)
+        plt.imshow(superimposed_img)
+        final_images.append(superimposed_img)
+        k += 3
+
+    return np.array(final_images)
 
 
 def load_data(data_directory):
@@ -105,3 +132,82 @@ def load_data(data_directory):
             pass
 
     return images
+
+
+def get_output_layers(net):
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+
+def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h, classes, COLORS):
+    label = str(classes[class_id])
+    color = COLORS[class_id]
+    cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+
+    cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+def detection(image, weights, cfg, classes_file):
+    weights = weights
+    config = cfg
+    image = cv2.imread(image)
+
+    Width = image.shape[1]
+    Height = image.shape[0]
+    scale = 0.00392
+
+    classes = None
+
+    with open(classes_file, 'r') as f:
+        classes = [line.strip() for line in f.readlines()]
+
+    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+
+    net = cv2.dnn.readNet(weights, config)
+
+    blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+
+    outs = net.forward(get_output_layers(net))
+
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.5
+    nms_threshold = 0.4
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        x = box[0]
+        y = box[1]
+        w = box[2]
+        h = box[3]
+        draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h), classes,
+                        COLORS)
+
+    return image, boxes
+
+
+
